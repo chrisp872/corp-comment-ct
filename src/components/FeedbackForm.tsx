e0638e829ad8de1b0a5b9794e799db982e13f8ef
Christopher G Parent <chris.parent@outlook.com>
2025-04-14 22:32:46 -0400

    WIP.  Now loading data from api.

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,19 +1,36 @@
-export default function FeedbackForm() {
-  return <form className="form">
-    <textarea 
-    id="feedback-textarea" 
-    placeholder="labelwillreplace" 
-    spellCheck={false} 
-    />
+import { useState } from "react";
+import { MAX_CHARACTERS } from "../lib/constants";
 
-    <label htmlFor="feedback-textarea">
-      Enter your feedback here, remember to #hashtag the company.
-    </label>
-    <div>
-      <p className="u-italic">150</p>
-      <button>
-        <span>Submit</span>
-      </button>
-    </div>
-  </form>;
+export default function FeedbackForm() {
+  const [text, setText] = useState("");
+  const charCount = MAX_CHARACTERS - text.length;
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const newText = e.target.value;
+    if (newText.length <= MAX_CHARACTERS) {
+      setText(newText);
+    }
+  };
+
+  return (
+    <form className="form">
+      <textarea
+        value={text}
+        onChange={handleChange}
+        id="feedback-textarea"
+        placeholder="labelwillreplace"
+        spellCheck={false}
+      />
+
+      <label htmlFor="feedback-textarea">
+        Enter your feedback here, remember to #hashtag the company.
+      </label>
+      <div>
+        <p className="u-italic">{charCount}</p>
+        <button>
+          <span>Submit</span>
+        </button>
+      </div>
+    </form>
+  );
 }
--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,23 +1,45 @@
-import { TriangleUpIcon } from "@radix-ui/react-icons";
+import { useEffect, useState } from "react";
+import FeedbackItem from "./FeedbackItem";
+import Spinner from "./Spinner";
+import ErrorMessage from "./ErrorMessage";
 
 export default function FeedbackList() {
-  return <ol className="feedback-list">
-    <li className="feedback">
-      <button>
-        <TriangleUpIcon />
-        <span>593</span>
-      </button>
-      <div>
-        <p>B</p>
-      </div>
+  const [feedbackItems, setFeedbackItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
-      <div>
-        <p>ByteGrad</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consequatur optio maxime iure, exercitationem debitis perferendis.</p>
-      </div>
+  useEffect(() => {
+    const fetchFeedbackItems = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(
+          "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok.");
+        }
+        const data = await response.json();
+        setFeedbackItems(data.feedbacks);
+      } catch (error) {
+        setErrorMessage(
+          "Failed to fetch feedback items. Please try again later."
+        );
+      }
+      setIsLoading(false);
+    };
 
-      <p>4d</p>
+    fetchFeedbackItems();
+  }, []);
 
-    </li>
-  </ol>;
+  return (
+    <ol className="feedback-list">
+      {isLoading && !errorMessage && <Spinner />}
+
+      {errorMessage && <ErrorMessage message={errorMessage} />}
+
+      {feedbackItems.map((feedbackItem) => (
+        <FeedbackItem key={feedbackItem.id} feedbackItem={feedbackItem} />
+      ))}
+    </ol>
+  );
 }
